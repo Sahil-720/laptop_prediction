52a1038a33c747b520e560a9e4ff568ca90a714f
Sahil-720 <sahilsatapathy2001@gmail.com>
2025-08-30 21:19:24 +0530

    Add files via upload

---
+++ smartprix.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+import time
+
+s = Service("C:/Users/sahil/OneDrive/Desktop/chromedriver.exe")
+
+driver = webdriver.Chrome(service=s)      # Launch Chrome
+driver.get("https://www.smartprix.com/laptops")
+time.sleep(2)
+
+#driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/aside/div/div[5]/div[2]/label[1]/input').click()
+#time.sleep(2)
+
+driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/aside/div/div[5]/div[2]/label[2]/input').click()
+time.sleep(2)
+
+driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/aside/div/div[5]/div[2]/label[4]/input').click()
+time.sleep(2)
+
+#driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/div[1]/div[2]/div[3]').click()
+#time.sleep(2)
+
+#driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/div[1]/div[2]/div[3]').click()
+#time.sleep(3)
+
+old_height = driver.execute_script('return document.body.scrollHeight')
+while True:
+
+    driver.find_element(by=By.XPATH, value='//*[@id="app"]/main/div[1]/div[2]/div[3]').click()
+    time.sleep(4)
+
+    new_height = driver.execute_script('return document.body.scrollHeight')
+
+    print(old_height)
+    print(new_height)
+
+    if new_height == old_height:
+        break
+
+    old_height = new_height
+time.sleep(5)
+html = driver.page_source
+
+with open('smartprix_24.html','w',encoding='utf-8') as f:
+    f.write(html)
